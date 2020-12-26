0364c902ba3ff0c91a7a80940f637420f80537d7
Fernando Augusto <f3rnand0_lp@hotmail.com>
2020-12-26 18:35:06 -0400

    Botão de Filtro adicionado

--- tasks/src/screens/TaskList.js
+++ tasks/src/screens/TaskList.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import { View, Text, ImageBackground, StyleSheet } from 'react-native';
+import { View, Text, ImageBackground, StyleSheet, TouchableOpacity, Platform } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 
 import commonStyles from '../commonStyles';
 import todayImage from '../../assets/imgs/today.jpg';
+
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 import moment from 'moment';
 import 'moment/locale/pt-br';
 
 import Task from '../components/Task';
-import { FlatList } from 'react-native-gesture-handler';
 
 export default class TaskList extends Component {
     
     state = {
+        showDoneTasks: true,
         tasks: [{
             id: Math.random(),
             desc: 'Comprar Livro de React Native',
@@ -24,6 +27,10 @@ export default class TaskList extends Component {
             estimateAt: new Date(),
             doneAt: null,
         }]
+    }
+    
+    toggleFilter = () => {
+        this.setState({ showDoneTasks: !this.state.showDoneTasks })
     }
     
     toggleTask = taskId => {
@@ -45,6 +52,12 @@ export default class TaskList extends Component {
         return (
             <View style={styles.container}>
                 <ImageBackground source={todayImage} style={styles.background}>
+                    <View style={styles.iconBar}>
+                        <TouchableOpacity onPress={this.toggleFilter}>
+                            <Icon name={this.state.showDoneTasks ? 'eye' : 'eye-slash'}
+                                size={20} color={ commonStyles.colors.secondary}/>
+                        </TouchableOpacity>
+                    </View>
                     <View style={styles.titleBar}>
                         <Text style={styles.title}>Hoje</Text>
                         <Text style={styles.subtitle}>{today}</Text>
@@ -90,4 +103,10 @@ const styles = StyleSheet.create({
         marginLeft: 20,
         marginBottom: 20,
     },
+    iconBar: {
+        flexDirection: 'row',
+        marginHorizontal: 20,
+        justifyContent: 'flex-end',
+        marginTop: Platform.OS === 'ios' ? 40 : 10
+    }
 });
