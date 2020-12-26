ca96c1b6d2f4228c8747cb647b3bdd514a6fb939
Fernando Augusto <f3rnand0_lp@hotmail.com>
2020-12-26 15:08:23 -0400

    Task Component e Estilo

--- tasks/src/screens/TaskList.js
+++ tasks/src/screens/TaskList.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {View, Text, ImageBackground, StyleSheet} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, ImageBackground, StyleSheet } from 'react-native';
 
 import commonStyles from '../commonStyles';
 import todayImage from '../../assets/imgs/today.jpg';
@@ -7,56 +7,57 @@ import todayImage from '../../assets/imgs/today.jpg';
 import moment from 'moment';
 import 'moment/locale/pt-br';
 
-export default class TaskList extends Component {
-  render() {
-    const today = moment()
-      .locale('pt-br')
-      .format('ddd, D [de] MMMM');
+import Task from '../components/Task';
 
-    return (
-      <View style={styles.container}>
-        <ImageBackground source={todayImage} style={styles.background}>
-          <View style={styles.titleBar}>
-            <Text style={styles.title}>Hoje</Text>
-            <Text style={styles.subtitle}>{today}</Text>
-          </View>
-        </ImageBackground>
-        <View style={styles.taskList}>
-          <Text>Tarefa #01</Text>
-          <Text>Tarefa #02</Text>
-          <Text>Tarefa #03</Text>
-        </View>
-      </View>
-    );
-  }
+export default class TaskList extends Component {
+    render() {
+        const today = moment()
+            .locale('pt-br')
+            .format('ddd, D [de] MMMM');
+
+        return (
+            <View style={styles.container}>
+                <ImageBackground source={todayImage} style={styles.background}>
+                    <View style={styles.titleBar}>
+                        <Text style={styles.title}>Hoje</Text>
+                        <Text style={styles.subtitle}>{today}</Text>
+                    </View>
+                </ImageBackground>
+                <View style={styles.taskList}>
+                    <Task desc="Comprar livro" estimateAt={new Date()} doneAt={new Date()} />
+                    <Task desc="Ler livro" estimateAt={new Date()} doneAt={null} />
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  background: {
-    flex: 3,
-  },
-  taskList: {
-    flex: 7,
-  },
-  titleBar: {
-    flex: 1,
-    justifyContent: 'flex-end',
-  },
-  title: {
-    fontFamily: commonStyles.fontFamily,
-    fontSize: 50,
-    color: commonStyles.colors.secondary,
-    marginLeft: 20,
-    marginBottom: 20,
-  },
-  subtitle: {
-    fontFamily: commonStyles.fontFamily,
-    color: commonStyles.colors.secondary,
-    fontSize: 20,
-    marginLeft: 20,
-    marginBottom: 20,
-  },
+    container: {
+        flex: 1,
+    },
+    background: {
+        flex: 3,
+    },
+    taskList: {
+        flex: 7,
+    },
+    titleBar: {
+        flex: 1,
+        justifyContent: 'flex-end',
+    },
+    title: {
+        fontFamily: commonStyles.fontFamily,
+        fontSize: 50,
+        color: commonStyles.colors.secondary,
+        marginLeft: 20,
+        marginBottom: 20,
+    },
+    subtitle: {
+        fontFamily: commonStyles.fontFamily,
+        color: commonStyles.colors.secondary,
+        fontSize: 20,
+        marginLeft: 20,
+        marginBottom: 20,
+    },
 });
