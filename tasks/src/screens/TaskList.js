da56bc81ef934b7c1f2c07689495a684456835e9
Fernando Augusto <f3rnand0_lp@hotmail.com>
2020-12-27 19:20:34 -0400

    Excluindo Tasks

--- tasks/src/screens/TaskList.js
+++ tasks/src/screens/TaskList.js
@@ -88,6 +88,11 @@ export default class TaskList extends Component {
         this.setState({ tasks, showAddTask: false }, this.filterTasks)
     }
     
+    deleteTask = id => {
+        const tasks = this.state.tasks.filter(task => task.id !== id)
+        this.setState({ tasks }, this.filterTasks)
+    }
+    
     render() {
         const today = moment()
             .locale('pt-br')
@@ -114,7 +119,8 @@ export default class TaskList extends Component {
                     <FlatList
                         data={this.state.visibleTasks}
                         keyExtractor={item => `${item.id}`}
-                        renderItem={({item}) => <Task {...item} toggleTask={this.toggleTask} /> }
+                        renderItem={({ item }) => <Task {...item} toggleTask={this.toggleTask}
+                           onDelete={this.deleteTask} />}
                     />
                 </View>
                 <TouchableOpacity style={styles.addButton}
