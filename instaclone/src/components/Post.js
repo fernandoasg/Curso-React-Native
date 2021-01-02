202d7a15a62c0784b5915f7af8c9fca3dcbca63e
Fernando Augusto <f3rnand0_lp@hotmail.com>
2021-01-02 13:07:56 -0400

    Tela de Feeds

--- instaclone/src/components/Post.js
+++ instaclone/src/components/Post.js
@@ -15,7 +15,8 @@ export default class Post extends Component {
         return (
             <View style={styles.container}>
                 <Image source={this.props.image} style={styles.image} />
-                <Author email='fernando@gmail.com' nickname="Fernando Augusto" />
+                <Author email={this.props.email} 
+                    nickname={this.props.nickname} />
                 <Comments comments={this.props.comments} />
                 <AddComment />
             </View>
